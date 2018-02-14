c3fd9cc764857d78ad80c994a50e161f11fe18da
Luis Camacho <lcamacho@iastate.edu>
2018-02-13 22:54:05 -0600

    testing return

--- php/getLink.php
+++ php/getLink.php
@@ -1,10 +1,10 @@
 <?php 
 	include 'mysql_Connection.php';
-	die('testing');
+	// die('testing');
 
 	$sqlGet = "SELECT * FROM VIDEODATALINKS";
 	$query = mysql_query($dbcon, $sqlGet) or die("Error getting info");
-	$results = mysql_fetch_assoc($query);
-    echo json_encode($results);
+	// $results = mysql_fetch_assoc($query);
+ //    echo json_encode($results);
 	// echo $results;
 ?>

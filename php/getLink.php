d1b0cb15f044cbb65d05d364af43bd5886e4de77
Luis Camacho <lcamacho@iastate.edu>
2018-02-13 23:35:42 -0600

    testing return

--- php/getLink.php
+++ php/getLink.php
@@ -2,10 +2,13 @@
 	include 'mysql_Connection.php';
 	// die('testing');
 
-	$sqlGet = "SELECT * FROM VIDEODATALINKS";
+	$sqlGet = "
+	SELECT * FROM VIDEODATALINKS
+	";
 	$query = mysqli_query($dbcon, $sqlGet) ;
 	// $results = mysqli_fetch_assoc($query);
     // echo json_encode($results);
     $results = mysqli_fetch_assoc($query);
-	print_r( $results );
+    echo $results['LINK'];
+	// print_r( $results );
 ?>

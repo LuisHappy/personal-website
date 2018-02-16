f24ab69d7017b1923d3d3dbe6e8f652d736f9424
Luis Camacho <lcamacho@iastate.edu>
2018-02-16 17:09:12 -0600

    Took out all defines and add them to getenv inside of config

--- php/getLink.php
+++ php/getLink.php
@@ -1,14 +1,9 @@
 <?php 
 	include 'mysql_Connection.php';
-	// die('testing');
-
 	$sqlGet = "
 	SELECT * FROM VIDEODATALINKS ORDER BY id DESC LIMIT 1;
 	";
 	$query = mysqli_query($dbcon, $sqlGet) ;
-	// $results = mysqli_fetch_assoc($query);
-    // echo json_encode($results);
     $results = mysqli_fetch_assoc($query);
     echo $results['LINK'];
-	// print_r( $results );
 ?>
--- php/mysql_Connection.php
+++ php/mysql_Connection.php
@@ -1,14 +1,4 @@
 <?php 
-	DEFINE ('DB_USER', 'root');
-	DEFINE ('DB_HOST', 'localhost');
-	DEFINE ('DB_PASS', 'password');
-	DEFINE ('DB_NAME', 'website');
-
+	include '../config/getenv.php';
 	$dbcon = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
-	// if (!$dbcon) {
-	// 	die("Erro connecting to database");
-	// } else {
-	// 	echo "You have connected succesfully";
-	// }
 ?>
